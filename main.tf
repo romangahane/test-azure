da185c93275582e56ed0e87b751141ca5d78f5b9
romangahane <59473978+romangahane@users.noreply.github.com>
2026-06-06 06:17:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,27 @@
+# Configure the Azure provider, you can have many
+# if you use azurerm provider, it's source is hashicorp/azurerm
+# short for registry.terraform.io/hashicorp/azurerm
+
+
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 4.70.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 4.8.0"
     }
   }
-  required_version = ">=1.9.0"
+
+  required_version = ">= 1.9.0"
 }
+# configures the provider
 
 provider "azurerm" {
-    features {
-  
-    }
-  
+  features {}
+  subscription_id = "55f61365-73d4-47a7-a591-631e3ce0d8de"
 }
-
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
- 
-  name                     = "romnathgahane"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location # implicit dependency
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
