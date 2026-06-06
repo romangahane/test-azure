c187d07b34dbd3638a47df726accabee763b3f7a
romangahane <59473978+romangahane@users.noreply.github.com>
2026-06-06 09:09:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,15 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+resource "azurerm_storage_account" "example" {
+ 
+  name                     = "gahaneromnathstz"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location # implicit dependency
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
